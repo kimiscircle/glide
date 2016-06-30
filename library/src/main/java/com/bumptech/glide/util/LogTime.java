af77f7c591293c83857beb8e018ff3ce0bd190e1
Jeffrey <kimiscircle@gmail.com>
2016-06-30 15:46:16 +0800

    fix compatibilty issue
    三星GALAXY SII	android 4.2.2

--- library/src/main/java/com/bumptech/glide/util/LogTime.java
+++ library/src/main/java/com/bumptech/glide/util/LogTime.java
@@ -21,11 +21,7 @@ public final class LogTime {
    */
   @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
   public static long getLogTime() {
-    if (Build.VERSION_CODES.JELLY_BEAN_MR1 <= Build.VERSION.SDK_INT) {
-      return SystemClock.elapsedRealtimeNanos();
-    } else {
       return System.currentTimeMillis();
-    }
   }
 
   /**
